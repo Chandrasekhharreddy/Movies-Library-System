3ba9d09db09b6871a9d0daf02223b5b4b3c596e4
Chandrasekhharreddy <144087291+Chandrasekhharreddy@users.noreply.github.com>
2024-06-05 11:53:58 +0530

    Added sample

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
 #jiii
 def function():
-    return "Hello World"
+    return render_template('Sample 0506.html')
 
 if __name__=='__main__':
     app.run(host="0.0.0.0",debug=True)
